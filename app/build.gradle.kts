23ef32816b9908ad3b3c3ea5a37021ec57040f4c
azurova <111108245+indigogal@users.noreply.github.com>
2026-09-10 18:26:50 -0700

    Dependency Update
    Added KSP and Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "2.3.10"
 }
 
 android {
@@ -36,7 +37,13 @@ android {
 }
 
 dependencies {
+    val room_version = "3.0.2"
+
+    implementation("androidx.room3:room3-runtime:$room_version")
+    ksp("androidx.room3:room3-compiler:$room_version")
+
     implementation("androidx.compose.ui:ui-text-google-fonts")
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
